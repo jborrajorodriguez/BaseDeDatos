48a0df03b2bbeed679c1986bd440f29cb08a028b
juan_ <juan_@DESKTOP-8JSFJJ3>
2018-05-22 11:54:29 +0200

    clase Funciones

--- src/basededatos/Funciones.java
+++ src/basededatos/Funciones.java
@@ -2,6 +2,8 @@ package basededatos;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,7 +13,7 @@ import java.util.logging.Logger;
  */
 public class Funciones {
     
-    static String url="C:\\Users\\Clase\\AppData\\Roaming\\NetBeans\\Derby\\Formula1";
+    static String url="C:\\Users\\juan_\\Downloads\\Sqliteman-1.2.2-win32\\Sqliteman-1.2.2\\F1.db";//"C:\\Users\\Clase\\AppData\\Roaming\\NetBeans\\Derby\\F1.db";
     static Connection conect;
     
     
@@ -30,9 +32,46 @@ public class Funciones {
     public static void cerrar(){
         try {
             conect.close();
+            System.out.println("Cerrado");
         } catch (SQLException ex) {
             System.out.println("No se puede cerrar");
         }
+    }
+    public static void guardarEquipo(Equipo equipo){
+        try {
+            PreparedStatement st = conect.prepareStatement("insert into equipo (codeq,nombre) values (?,?)");
+            st.setString(1, equipo.getCodeq());
+            st.setString(2, equipo.getNombre());
+            st.execute();
+        } catch (SQLException ex) {
+            Logger.getLogger(Funciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
+    
+    public static void mostrarEquipos(){
+        
+        try {
+            ResultSet result=null;
+            PreparedStatement st = conect.prepareStatement("select * from equipo");
+            result = st.executeQuery();
+            //System.out.println(result);
+            while (result.next()) {
+                System.out.print("codeq: ");
+                System.out.println(result.getString("codeq"));
+ 
+                System.out.print("Nombre: ");
+                System.out.println(result.getString("Nombre"));
+
+
+ 
+                System.out.println("=======================");
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(Funciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+            
+        
     }
 
 }
