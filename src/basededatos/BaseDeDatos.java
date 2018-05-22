4839d144d0d4a6be490152569873099d61321eb2
Juan Borrajo <juanborrajo10@gmail.com>
2018-05-22 09:34:29 +0200

    main

---
+++ src/basededatos/BaseDeDatos.java
@@ -0,0 +1,16 @@
+
+package basededatos;
+
+/**
+ *
+ * @author Juan Borrajo Rodriguez
+ */
+public class BaseDeDatos {
+
+    
+    public static void main(String[] args) {
+        Funciones.conectar();
+        //Funciones.cerrar();
+    }
+
+}
