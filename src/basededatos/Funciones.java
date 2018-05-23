5408e80ab9b8d196073c28938ba606daa63da06e
Juan Borrajo <juanborrajo10@gmail.com>
2018-05-23 08:33:46 +0200

    Clase funcion

--- src/basededatos/Funciones.java
+++ src/basededatos/Funciones.java
@@ -12,66 +12,75 @@ import java.util.logging.Logger;
  * @author {Juan Borrajo Rodriguez}
  */
 public class Funciones {
-    
-    static String url="C:\\Users\\juan_\\Downloads\\Sqliteman-1.2.2-win32\\Sqliteman-1.2.2\\F1.db";//"C:\\Users\\Clase\\AppData\\Roaming\\NetBeans\\Derby\\F1.db";
-    static Connection conect;
-    
-    
-    public static void conectar(){
+
+    static String url = "E:\\Sqliteman-1.2.2\\F1.db";//"C:\\Users\\Clase\\AppData\\Roaming\\NetBeans\\Derby\\F1.db";
+    static Connection connect;
+
+    public static void conectar() {
         try {
-            conect=DriverManager.getConnection("jdbc:sqlite:"+url);
-            if (conect!=null) {
+            connect = DriverManager.getConnection("jdbc:sqlite:" + url);
+            if (connect != null) {
                 System.out.println("Conectado");
-                
+
             }
         } catch (SQLException ex) {
             System.out.println("No se puede conectar con la base de datos");
         }
     }
-    
-    public static void cerrar(){
+
+    public static void cerrar() {
         try {
-            conect.close();
+            connect.close();
             System.out.println("Cerrado");
         } catch (SQLException ex) {
             System.out.println("No se puede cerrar");
         }
     }
-    public static void guardarEquipo(Equipo equipo){
+
+    public static void guardarEquipo(Equipo equipo) {
         try {
-            PreparedStatement st = conect.prepareStatement("insert into equipo (codeq,nombre) values (?,?)");
+            PreparedStatement st = connect.prepareStatement("insert into equipo (codeq,nombre) values (?,?)");
             st.setString(1, equipo.getCodeq());
             st.setString(2, equipo.getNombre());
             st.execute();
         } catch (SQLException ex) {
             Logger.getLogger(Funciones.class.getName()).log(Level.SEVERE, null, ex);
         }
-        
+
     }
-    
-    public static void mostrarEquipos(){
+
+    public static void mostrarEquipos() {
         
+
+        Equipo equipo = null;
+        ResultSet result = null;
         try {
-            ResultSet result=null;
-            PreparedStatement st = conect.prepareStatement("select * from equipo");
+
+            PreparedStatement st = connect.prepareStatement("select * from equipo");
             result = st.executeQuery();
+
             //System.out.println(result);
             while (result.next()) {
                 System.out.print("codeq: ");
                 System.out.println(result.getString("codeq"));
- 
+
+                
+
                 System.out.print("Nombre: ");
                 System.out.println(result.getString("Nombre"));
+              
 
-
- 
+               
                 System.out.println("=======================");
             }
+
         } catch (SQLException ex) {
             Logger.getLogger(Funciones.class.getName()).log(Level.SEVERE, null, ex);
         }
-            
         
+
     }
+
+    
 
 }
