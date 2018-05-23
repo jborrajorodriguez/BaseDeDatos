6d52435b37e7c6bdab485bcf869fa11c9ac6fc39
Juan Borrajo <juanborrajo10@gmail.com>
2018-05-23 09:26:42 +0200

    Clase main

--- src/basededatos/BaseDeDatos.java
+++ src/basededatos/BaseDeDatos.java
@@ -11,11 +11,14 @@ public class BaseDeDatos {
 
     
     public static void main(String[] args) {
-       // Funciones.conectar();
+        Funciones.conectar();
         //Funciones.mostrarEquipos();
-        //Funciones.guardarEquipo(new Equipo("3","Renault"));
-       // Funciones.mostrarEquipos();
-       // Funciones.cerrar();
+      //Funciones.guardarEquipo(new Equipo("3","Renault","Nico Hulkenberg","Carlos Sainz"));
+       //Funciones.actualizarEquipo("piloto2","Juan Borrajo","3");
+       //Funciones.mostrarEquipos();
+       //Funciones.borrarEquipo("3");
+       Funciones.mostrarEquipos();
+        Funciones.cerrar();
        
         
     }
