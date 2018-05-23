2b8f7356f6fae2345e0dd15ab08a413d0244d8f7
Juan Borrajo <juanborrajo10@gmail.com>
2018-05-23 09:32:28 +0200

    Clase Funciones

--- src/basededatos/Funciones.java
+++ src/basededatos/Funciones.java
@@ -13,7 +13,7 @@ import java.util.logging.Logger;
  */
 public class Funciones {
 
-    static String url = "E:\\Sqliteman-1.2.2\\F1.db";//"C:\\Users\\Clase\\AppData\\Roaming\\NetBeans\\Derby\\F1.db";
+    static String url = "E:\\Sqliteman-1.2.2\\F1.db";
     static Connection connect;
 
     public static void conectar() {
@@ -39,20 +39,66 @@ public class Funciones {
 
     public static void guardarEquipo(Equipo equipo) {
         try {
-            PreparedStatement st = connect.prepareStatement("insert into equipo (codeq,nombre) values (?,?)");
+            PreparedStatement st = connect.prepareStatement("insert into equipo (codeq,nombre,piloto1,piloto2) values (?,?,?,?)");
             st.setString(1, equipo.getCodeq());
             st.setString(2, equipo.getNombre());
+            st.setString(3, equipo.getPiloto1());
+            st.setString(4, equipo.getPiloto2());
+            
             st.execute();
         } catch (SQLException ex) {
             Logger.getLogger(Funciones.class.getName()).log(Level.SEVERE, null, ex);
         }
 
     }
+    
+    public static void borrarEquipo(String codeq){
+        
+        try {
+            
+            PreparedStatement st = connect.prepareStatement("delete from equipo where codeq="+"'"+codeq
+            +"'");
+            st.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Funciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void actualizarEquipo(String campo,String dato,String codeq){
+        try {
+            PreparedStatement st = connect.prepareStatement("update equipo set "+campo+"="+"'"+dato+"' where codeq='"+codeq+"'");
+            st.executeUpdate();
+        } catch (SQLException ex) {
+            Logger.getLogger(Funciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public static void buscar(String codeq){
+        try {
+            ResultSet result = null;
+            PreparedStatement st = connect.prepareStatement("select * from equipo where codeq='"+codeq+"'");
+            result = st.executeQuery();
+            System.out.print("codeq: ");
+                System.out.println(result.getString("codeq"));
+
+                System.out.print("Nombre: ");
+                System.out.println(result.getString("Nombre"));
+                
+                System.out.print("Piloto 1: ");
+                System.out.println(result.getString("piloto1"));
+                
+                System.out.print("Piloto 2: ");
+                System.out.println(result.getString("piloto2"));
+        } catch (SQLException ex) {
+            Logger.getLogger(Funciones.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+    }
 
     public static void mostrarEquipos() {
         
 
-        Equipo equipo = null;
+        
         ResultSet result = null;
         try {
 
@@ -64,10 +110,17 @@ public class Funciones {
                 System.out.print("codeq: ");
                 System.out.println(result.getString("codeq"));
 
-                
-
                 System.out.print("Nombre: ");
                 System.out.println(result.getString("Nombre"));
+                
+                System.out.print("Piloto 1: ");
+                System.out.println(result.getString("piloto1"));
+                
+                System.out.print("Piloto 2: ");
+                System.out.println(result.getString("piloto2"));
+                
+                
+                
               
 
                
